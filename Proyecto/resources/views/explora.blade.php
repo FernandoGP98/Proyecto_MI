f9927b4688fe614274f3f7eae9f1105128e5edc5
Fernando <fer_char98@hotmail.com>
2020-02-28 13:43:07 -0600

    quitar https

--- Proyecto/resources/views/explora.blade.php
+++ Proyecto/resources/views/explora.blade.php
@@ -4,14 +4,14 @@
     <div class="row d-flex justify-content-center">
         <div class="col-md-9">
             <div class="slider-for mx-auto">
-                <img class="img-responsive" src="https://i.ytimg.com/vi/HaGkk60kcjQ/maxresdefault.jpg" alt="" srcset="" width="412px" height="232px">
-                <img class="img-responsive" src="https://i.ytimg.com/vi/HaGkk60kcjQ/maxresdefault.jpg" alt="" srcset="" width="412px" height="232px">
+                <img class="img-responsive" src="//i.ytimg.com/vi/HaGkk60kcjQ/maxresdefault.jpg" alt="" srcset="" width="412px" height="232px">
+                <img class="img-responsive" src="//i.ytimg.com/vi/HaGkk60kcjQ/maxresdefault.jpg" alt="" srcset="" width="412px" height="232px">
                 <img class="img-responsive" src="https://i.ytimg.com/vi/HaGkk60kcjQ/maxresdefault.jpg" alt="" srcset="" width="412px" height="232px">
             </div>
             <div class="slider-nav mx-auto">
-                <img class="img-responsive" src="https://i.ytimg.com/vi/HaGkk60kcjQ/maxresdefault.jpg" alt="" srcset="" width="206px" height="116px">
-                <img class="img-responsive" src="https://i.ytimg.com/vi/HaGkk60kcjQ/maxresdefault.jpg" alt="" srcset="" width="206px" height="116px">
-                <img class="img-responsive" src="https://i.ytimg.com/vi/HaGkk60kcjQ/maxresdefault.jpg" alt="" srcset="" width="206px" height="116px">
+                <img class="img-responsive" src="//i.ytimg.com/vi/HaGkk60kcjQ/maxresdefault.jpg" alt="" srcset="" width="206px" height="116px">
+                <img class="img-responsive" src="//i.ytimg.com/vi/HaGkk60kcjQ/maxresdefault.jpg" alt="" srcset="" width="206px" height="116px">
+                <img class="img-responsive" src="//i.ytimg.com/vi/HaGkk60kcjQ/maxresdefault.jpg" alt="" srcset="" width="206px" height="116px">
             </div>
         </div>
     </div>
--- Proyecto/resources/views/home.blade.php
+++ Proyecto/resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="{{ asset('/css/home.css') }}">
 <div class="row">
     <div class="col-md-12 landscape">
-        <img src="https://images.squarespace-cdn.com/content/v1/540df639e4b01d517a0773d3/1495522769075-69WVF1M0EQVKL1AXDZJ3/ke17ZwdGBToddI8pDm48kCfD5VhG_v4AoG9r1szEQU0UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYwL8IeDg6_3B-BRuF4nNrNcQkVuAT7tdErd0wQFEGFSnBlmxMwF-qJcfq6dw5jYfi6uK6E-0yWJ134SXwDjkOVdC10wnnoXfSNyKpQymiZEVw/sailing+past+Tahiti+copy.jpg?format=1000w"
+        <img src="//images.squarespace-cdn.com/content/v1/540df639e4b01d517a0773d3/1495522769075-69WVF1M0EQVKL1AXDZJ3/ke17ZwdGBToddI8pDm48kCfD5VhG_v4AoG9r1szEQU0UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYwL8IeDg6_3B-BRuF4nNrNcQkVuAT7tdErd0wQFEGFSnBlmxMwF-qJcfq6dw5jYfi6uK6E-0yWJ134SXwDjkOVdC10wnnoXfSNyKpQymiZEVw/sailing+past+Tahiti+copy.jpg?format=1000w"
         alt="Imagen grandota del juego" width="100%" style="object-fit: cover;" >
         <h1 style="color:aliceblue" class="centered">The three elements</h1>
     </div>
@@ -11,7 +11,7 @@
 <div class="story">
     <div class="story-bg"></div>
     <div class="col-md-6 text-center story-cover">
-        <img src="https://cdna.artstation.com/p/assets/images/images/016/448/126/large/jeryce-dianingana-long-main.jpg?1552219180"
+        <img src="//cdna.artstation.com/p/assets/images/images/016/448/126/large/jeryce-dianingana-long-main.jpg?1552219180"
         alt="Otra imagen bonita" srcset="" width="auto" height="90%">
         <h4 class="bottom-right">La<br>Historia</h4>
     </div>
