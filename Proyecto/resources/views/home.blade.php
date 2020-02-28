569cd9a3adfd0dcb8259b5016c71fcef2209ba98
Fernando <fer_char98@hotmail.com>
2020-02-28 13:59:04 -0600

    HTTPS

--- Proyecto/resources/views/home.blade.php
+++ Proyecto/resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.index')
 @section('content')
-<link rel="stylesheet" href="{{ asset('/css/home.css') }}">
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
 <div class="row">
     <div class="col-md-12 landscape">
         <img src="//images.squarespace-cdn.com/content/v1/540df639e4b01d517a0773d3/1495522769075-69WVF1M0EQVKL1AXDZJ3/ke17ZwdGBToddI8pDm48kCfD5VhG_v4AoG9r1szEQU0UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYwL8IeDg6_3B-BRuF4nNrNcQkVuAT7tdErd0wQFEGFSnBlmxMwF-qJcfq6dw5jYfi6uK6E-0yWJ134SXwDjkOVdC10wnnoXfSNyKpQymiZEVw/sailing+past+Tahiti+copy.jpg?format=1000w"
